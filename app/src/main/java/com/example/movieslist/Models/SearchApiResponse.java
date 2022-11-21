8c90325eafe6f6c5aac90a8b013e7060ee7acc36
Tchoco <Siby.Ousmane@outlook.fr>
2022-11-21 10:52:43 +0100

    Add first stage to use SearchMovies API function

---
+++ app/src/main/java/com/example/movieslist/Models/SearchApiResponse.java
@@ -0,0 +1,16 @@
+package com.example.movieslist.Models;
+
+import java.util.List;
+
+public class SearchApiResponse
+{
+    List<SearchArrayObjectsForSearchMovies> results = null;
+
+    public List<SearchArrayObjectsForSearchMovies> getResults() {
+        return results;
+    }
+
+    public void setResults(List<SearchArrayObjectsForSearchMovies> results) {
+        this.results = results;
+    }
+}
